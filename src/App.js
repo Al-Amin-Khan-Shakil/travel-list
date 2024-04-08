c4a23b11101cb8b7ad266c735bcb1a9dfac3fd3c
Al-Amin <alaminkhanshakil@gmail.com>
2024-04-08 14:45:23 +0600

    Form added to create new items

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
+import { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Form from './components/Form';
 import PackingList from './components/PackingList';
 import Footer from './components/Footer';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const storedItems = localStorage.getItem('items');
+
+    if (storedItems) {
+      setItems(JSON.parse(storedItems));
+    }
+  }, []);
+
+  const handleAddItems = (newItem) => {
+    const updatedItems = [...items, newItem];
+    setItems(updatedItems);
+    localStorage.setItem('items', JSON.stringify(updatedItems));
+  };
+
   return (
     <div className="App">
       <Header />
-      <Form />
+      <Form onAddItems={handleAddItems} items />
       <PackingList />
       <Footer />
     </div>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,50 @@
-export default function Form() {
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { PropTypes } from 'prop-types';
+
+export default function Form({ onAddItmes }) {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
+  const handleSubnit = (e) => {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = {
+      description,
+      quantity,
+      packed: false,
+      id: uuidv4(),
+    };
+
+    onAddItmes(newItem);
+  };
+
   return (
-    <h3>What do you need for your 😍 trip?</h3>
+    <form onSubmit={handleSubnit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Items..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button type="submit">Add</button>
+    </form>
   );
 }
+
+Form.propTypes = {
+  onAddItmes: PropTypes.func.isRequired,
+};
